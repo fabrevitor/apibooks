c363b7b5347231b7da1a61bcce488d670161e544
fabrevitor <vitor_fabre@hotmail.com>
2021-07-16 23:52:37 -0300

    Create project

---
+++ src/test/java/com/zup/apibooks/ApibooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.apibooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
