a93b5d6fea7cd5c62a15a671b674d39d7e72aa1b
fabrevitor <vitor_fabre@hotmail.com>
2021-07-17 01:14:32 -0300

    Criando services e métodos findall e findbyid

--- src/main/java/com/zup/apibooks/resources/UserResource.java
+++ src/main/java/com/zup/apibooks/resources/UserResource.java
@@ -1,21 +1,37 @@
 package com.zup.apibooks.resources;
 
-import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zup.apibooks.entities.User;
+import com.zup.apibooks.services.UserService;
+
+//UserResource - Controller - Deixando de forma mais enxuta e levando as regras de negócio pra camada de Service
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User>findAll(){
-		User u = new User(1L, "Vitor", "vitor_fabre@hotmail.com", "111.854.349-12", LocalDate.now());
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>>findAll(){
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	// Definindo uma busca de Usuário por id, utilizando o value no GetMapping e o PathVariable para especificar a url para permitir um id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/zup/apibooks/services/UserService.java
@@ -0,0 +1,27 @@
+package com.zup.apibooks.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zup.apibooks.entities.User;
+import com.zup.apibooks.repository.UserRepository;
+
+//UserService - Controller - Regras de negócio do controller
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
